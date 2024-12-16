cf708646249504981be5595e8e639cbda394ba92
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-12-16 14:02:22 -0500

    iusfhius

--- app/src/main.js
+++ app/src/main.js
@@ -25,7 +25,40 @@ async function card() {
       let yourcard2 = deck.cards[1];
       console.log(yourcard2);
 
-      document.querySelector(".container");
+      document.querySelector(".container").insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class = "yourcards">
+          <img src = "${yourcard1.image}" class = "card-image" alt = "card1">
+          <img src = "${yourcard2.image}" class = "card-image" alt = "card1">
+        </div>
+        `
+      );
+      if (
+        yourcard2.value !== "ACE" &&
+        yourcard1.value !== "KING" &&
+        yourcard1.value !== "QUEEN" &&
+        yourcard1.value !== "JACK"
+      ) {
+        Number(yourcard1.value);
+        console.log(yourcard1.value);
+      }
+      if (
+        yourcard2.value !== "ACE" &&
+        yourcard2.value !== "KING" &&
+        yourcard2.value !== "QUEEN" &&
+        yourcard2.value !== "JACK"
+      ) {
+        Number(yourcard2.value);
+        console.log(yourcard2.value);
+      }
+      if (
+        typeof yourcard1.value === Number &&
+        typeof yourcard2.value === Number
+      ) {
+        let value = yourcard1.value + yourcard2.value;
+        console.log(value);
+      }
     }
   } catch (error) {
     alert("hey I could not find that agent unc");
