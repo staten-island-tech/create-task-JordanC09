554e1c1fbecb3f0f473ffc71f8896bc0cbf65d6a
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-01-03 14:00:56 -0500

    fgnngf

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,39 @@
 import "./style.css";
+let value1;
+let value2;
+
+function checkvalue(yourcard) {
+  let cardvalue;
+  if (
+    yourcard.value !== "ACE" &&
+    yourcard.value !== "KING" &&
+    yourcard.value !== "QUEEN" &&
+    yourcard.value !== "JACK"
+  ) {
+    cardvalue = yourcard.value;
+    cardvalue = Number(cardvalue);
+    return cardvalue;
+  }
+
+  if (
+    yourcard.value === "KING" ||
+    yourcard.value === "QUEEN" ||
+    yourcard.value === "JACK"
+  ) {
+    cardvalue = 10;
+
+    console.log(typeof cardvalue);
+    return cardvalue;
+  }
+
+  if (yourcard.value === "ACE") {
+    cardvalue = 11;
+
+    console.log(typeof cardvalue);
+    return cardvalue;
+  }
+}
+
 async function card() {
   try {
     const response = await fetch(
@@ -25,55 +60,9 @@ async function card() {
       let yourcard2 = deck.cards[1];
       console.log(yourcard2);
 
-      let value1;
-      let value2;
+      value1 = checkvalue(yourcard1);
+      value2 = checkvalue(yourcard2);
 
-      document.querySelector(".container").insertAdjacentHTML(
-        "beforeend",
-        `
-        <div class = "yourcards">
-          <img src = "${yourcard1.image}" class = "card-image" alt = "card1">
-          <img src = "${yourcard2.image}" class = "card-image" alt = "card1">
-        </div>
-        `
-      );
-      if (
-        yourcard2.value !== "ACE" &&
-        yourcard1.value !== "KING" &&
-        yourcard1.value !== "QUEEN" &&
-        yourcard1.value !== "JACK"
-      ) {
-        value1 = yourcard1.value;
-        value1 = Number(value1);
-      }
-      if (
-        yourcard2.value !== "ACE" &&
-        yourcard2.value !== "KING" &&
-        yourcard2.value !== "QUEEN" &&
-        yourcard2.value !== "JACK"
-      ) {
-        value2 = yourcard2.value;
-        value2 = Number(value2);
-      }
-
-      if (
-        yourcard1.value === "KING" ||
-        yourcard1.value === "QUEEN" ||
-        yourcard1.value === "JACK"
-      ) {
-        value1 = 10;
-
-        console.log(typeof value1);
-      }
-      if (
-        yourcard2.value === "KING" ||
-        yourcard2.value === "QUEEN" ||
-        yourcard2.value === "JACK"
-      ) {
-        value2 = 10;
-      }
-
-      console.log(value1);
       if ((typeof value1 === "number") & (typeof value2 === "number")) {
         let WhyisLaithcopyingme = value1 + value2;
         console.log(WhyisLaithcopyingme);
@@ -86,6 +75,55 @@ async function card() {
           `
         );
       }
+
+      document.querySelector(".container").insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class = "yourcards">
+          <img src = "${yourcard1.image}" class = "card-image" alt = "card1">
+          <img src = "${yourcard2.image}" class = "card-image" alt = "card1">
+        </div>
+        `
+      );
+
+      const enemy = await fetch(
+        `https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=2`
+      );
+      deck = await enemy.json();
+
+      let enemycard1 = deck.cards[0];
+      console.log(enemycard1);
+
+      let enemycard2 = deck.cards[1];
+      console.log(enemycard2);
+
+      enemyvalue1 = checkvalue(enemycard1);
+      enemyvalue2 = checkvalue(enemycard2);
+
+      if (
+        (typeof enemyvalue1 === "number") &
+        (typeof enemyvalue2 === "number")
+      ) {
+        let Laithisabum = enemyvalue1 + enemyvalue2;
+        console.log(Laithisabum);
+        document.querySelector(".container").insertAdjacentHTML(
+          "beforeend",
+          `
+          <div class = "Total">
+            <h2>${Laithisabum}</h2>
+          </div>
+          `
+        );
+      }
+      document.querySelector(".container").insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class = "enemycards">
+          <img src = "${enemycard1.image}" class = "enemycard-image" alt = "enemycard1">
+          <img src = "${enemycard2.image}" class = "enemycard-image" alt = "enemycard1">
+        </div>
+        `
+      );
     }
   } catch (error) {
     alert("hey I could not find that agent unc");
